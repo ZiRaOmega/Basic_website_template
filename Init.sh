3f1f9c5d7a24b85951636d6704652e8ad3cdb8f4
ZiRaOmega <max.diet@free.fr>
2022-12-03 04:39:29 +0000

    Add 'Init.sh'

---
+++ Init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create project folder
+mkdir project
+cd project
+
+# Create files and folders
+touch index.html
+mkdir css
+touch css/styles.css
+mkdir js
+touch js/script.js
+
+# Print success message
+echo "Project folder initialized!"
